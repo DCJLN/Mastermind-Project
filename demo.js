bb7de334b8dc2b162c5c9afd34b5a95765e943ba
DCJLN <38653678+DCJLN@users.noreply.github.com>
2018-05-05 18:31:54 +0200

    Add files via upload

---
+++ demo.js
@@ -0,0 +1,12 @@
+
+// alert("ERROR: \n You are not allowed for accessing this page");
+
+// prompt("Please enter your name", "ex: Dylan");
+
+var result = confirm("Do you really want to leave this page?");
+if (result == true) {
+  alert("Thanks for visiting");
+}
+else {
+  alert("Thanks for staying with us");
+}
