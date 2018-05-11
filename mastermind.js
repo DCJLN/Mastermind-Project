d80aa132fd6a534e2992f8643bc6b8cdb717e93a
DCJLN <38653678+DCJLN@users.noreply.github.com>
2018-05-11 13:54:56 +0200

    ID for guessdot & tipdot + javascript change (pin modification function + optionnal guess modification function)

--- mastermind.js
+++ mastermind.js
@@ -1,6 +1,7 @@
-
-
-for(i = 1; i <= 12; i++){
+/*
+ * //// Si javascript activé ////
+ */
+/*for(i = 0; i < 12; i++){
 							
 	var newMainDiv = document.createElement("div")
 	newMainDiv.setAttribute("class", "guess-tip-container")
@@ -18,39 +19,34 @@ for(i = 1; i <= 12; i++){
 	document.getElementById("containertorepeat"+i).appendChild(newGuessDiv)
 	document.getElementById("containertorepeat"+i).appendChild(newTipDiv)
 
-	for(j = 1; j <= 4; j++){
+	for(j = 0; j < 4; j++){
 		var newSpan = document.createElement("span")
 		newSpan.setAttribute("class", "guess-dot")
 		newSpan.setAttribute("id", "guessdot"+i+j)
 		document.getElementById("guess-cont"+i).appendChild(newSpan)
 	}
 
-	for(k = 1; k <= 4; k++){
+	for(k = 0; k < 4; k++){
 		var newSpan = document.createElement("span")
 		newSpan.setAttribute("class", "tip-dot")
 		newSpan.setAttribute("id", "tipdot"+i+k)
 		document.getElementById("tip-cont"+i).appendChild(newSpan)
 	}
-}
-
+}*/
 
 
 // colors: RED BLUE YELLOW GREEN WHITE BLACK
-var colors = [
-"rgb(231, 76, 60)",
-"rgb(52, 152, 219)",
-"rgb(241, 196, 15)",
-"rgb(46, 204, 113)",
-"white",
-"black"];
+var buttons = [];
+var colors = ["red", "blue", "yellow", "green", "white", "black"];
 
 function cycleColor(buttonID){
 	el = document.getElementById(buttonID);
+	el.classList.remove(colors[el.color]);
 	el.color = (el.color+1)%6;
-	el.style.backgroundColor = colors[el.color];
+	el.classList.add(colors[el.color]);
 }
 
-var row = 1
+// var row = 0;
 function submitCombination(){
 	var buttons = [];
 	for(i = 1; i<= 4; i++)
@@ -63,28 +59,30 @@ function submitCombination(){
 	console.log(url);
 	httpGet(url, processResponse);
 
-	/* A ajouter */
-	for(col = 1; col <= 4; col++){
-		guessModification("guessdot"+row+col, "button"+col)
+/*
+ * //// Si javascript activé ////
+ */
+/*
+	for(col = 0; col < 4; col++){
+		guessModification("guessdot"+row+col, "button"+(col+1))
 	}
 	row++
-	//row = (row + 1)%12 // %12 -> Est ce réellement nécessaire si le jeu s'arrete à la fin.
-
+*/
 }
 
-/* A ajouter */
+/*
 function guessModification(guessDotId, buttonID){
 	guess = document.getElementById(guessDotId)
 	button = document.getElementById(buttonID)
 	guess.style.backgroundColor = button.style.backgroundColor
 }
+*/
 
 window.onload = function(){
-	var buttons = [];
 	for(i = 1; i<= 4; i++)
 		buttons.push(document.getElementById("button"+i));
 	for(b of buttons)
-		b.color = colors.indexOf(b.style.backgroundColor);
+		b.color = colors.indexOf(b.classList[1]);
 }
 
 function httpGet(url, callback){
@@ -97,21 +95,30 @@ function httpGet(url, callback){
     xmlHttp.send(null);
 }
 
+var row = 0
 function processResponse(request){
 	var correct = request.getResponseHeader("Correct");
 	var misplaced = request.getResponseHeader("Misplaced");
-	console.log("correct: "+correct);
-	console.log("misplaced: "+misplaced);
+	console.log("correct: "+correct)
+	console.log("misplaced: "+misplaced)
+	tipModification(correct, misplaced)
 }
 
-/*
 function tipModification(correct, misplaced){
 	nbCorrect = correct
 	nbMisplaced = misplaced
-	tip = document.getElementById()
+	hexaRow = (row).toString(16)
 
-	for(i = 1; i <= 4; i++){
-		tipdot
-		if 
+	for(i = 0; i < 4; i++){
+		tip = document.getElementById("tipdot"+hexaRow+i)
+		if(nbCorrect > 0){
+			tip.classList.replace("&", "white")
+			nbCorrect--
+		}
+		else if(nbMisplaced > 0){
+			tip.classList.replace("&", "black")
+			nbMisplaced--
+		}
 	}
-*/
+	row = (row + 1)%12
+}
