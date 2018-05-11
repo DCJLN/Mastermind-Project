5e5691a8d8b161ff916e737aa1b8f46a75434eaf
DCJLN <38653678+DCJLN@users.noreply.github.com>
2018-05-11 13:51:19 +0200

    ID for guessdot & tipdot + javascript change (pin modification function + optionnal guess modification function)

--- mastermind.js
+++ mastermind.js
@@ -1,4 +1,6 @@
-/*for(i = 0; i < 12; i++){
+
+
+for(i = 1; i <= 12; i++){
 							
 	var newMainDiv = document.createElement("div")
 	newMainDiv.setAttribute("class", "guess-tip-container")
@@ -16,34 +18,39 @@
 	document.getElementById("containertorepeat"+i).appendChild(newGuessDiv)
 	document.getElementById("containertorepeat"+i).appendChild(newTipDiv)
 
-	for(j = 0; j < 4; j++){
+	for(j = 1; j <= 4; j++){
 		var newSpan = document.createElement("span")
 		newSpan.setAttribute("class", "guess-dot")
 		newSpan.setAttribute("id", "guessdot"+i+j)
 		document.getElementById("guess-cont"+i).appendChild(newSpan)
 	}
 
-	for(k = 0; k < 4; k++){
+	for(k = 1; k <= 4; k++){
 		var newSpan = document.createElement("span")
 		newSpan.setAttribute("class", "tip-dot")
 		newSpan.setAttribute("id", "tipdot"+i+k)
 		document.getElementById("tip-cont"+i).appendChild(newSpan)
 	}
-}*/
+}
 
 
 
 // colors: RED BLUE YELLOW GREEN WHITE BLACK
-var buttons = [];
-var colors = ["red", "blue", "yellow", "green", "white", "black"];
+var colors = [
+"rgb(231, 76, 60)",
+"rgb(52, 152, 219)",
+"rgb(241, 196, 15)",
+"rgb(46, 204, 113)",
+"white",
+"black"];
 
 function cycleColor(buttonID){
 	el = document.getElementById(buttonID);
-	el.classList.remove(colors[el.color]);
 	el.color = (el.color+1)%6;
-	el.classList.add(colors[el.color]);
+	el.style.backgroundColor = colors[el.color];
 }
 
+var row = 1
 function submitCombination(){
 	var buttons = [];
 	for(i = 1; i<= 4; i++)
@@ -55,13 +62,29 @@ function submitCombination(){
 	url = url.slice(0,-1);
 	console.log(url);
 	httpGet(url, processResponse);
+
+	/* A ajouter */
+	for(col = 1; col <= 4; col++){
+		guessModification("guessdot"+row+col, "button"+col)
+	}
+	row++
+	//row = (row + 1)%12 // %12 -> Est ce réellement nécessaire si le jeu s'arrete à la fin.
+
+}
+
+/* A ajouter */
+function guessModification(guessDotId, buttonID){
+	guess = document.getElementById(guessDotId)
+	button = document.getElementById(buttonID)
+	guess.style.backgroundColor = button.style.backgroundColor
 }
 
 window.onload = function(){
+	var buttons = [];
 	for(i = 1; i<= 4; i++)
 		buttons.push(document.getElementById("button"+i));
 	for(b of buttons)
-		b.color = colors.indexOf(b.classList[1]);
+		b.color = colors.indexOf(b.style.backgroundColor);
 }
 
 function httpGet(url, callback){
@@ -80,3 +103,15 @@ function processResponse(request){
 	console.log("correct: "+correct);
 	console.log("misplaced: "+misplaced);
 }
+
+/*
+function tipModification(correct, misplaced){
+	nbCorrect = correct
+	nbMisplaced = misplaced
+	tip = document.getElementById()
+
+	for(i = 1; i <= 4; i++){
+		tipdot
+		if 
+	}
+*/
